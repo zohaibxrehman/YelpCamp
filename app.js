5a3e76c30d3438fb9b04a5f3cc86f1dad65fdf87
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-07 17:05:54 -0400

    Added show page for camps. Desc added to mongoDB. Renamed campground ejs to index.

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
-var express 	= require('express'),
-	app 		= express(),
-	bodyParser 	= require('body-parser'),
-	mongoose 	= require('mongoose');
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true});
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: 'Salmon Creek',
+// 		image:
+// 			'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e5074417c2772d4964fc2_340',
+// 		description: 'For those that seek adventure amongst the wild! Kayaking and rock climbing included.'
+// 	},
+// 	function(err, campground) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log('NEWLY CREATED CAMPGROUND: ');
+// 			console.log(campground);
+// 		}
+// 	}
+// );
 
 app.get('/', function(req, res) {
 	res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-	Campground.find({}, function(err, campgrounds){
-		if(err){
+	Campground.find({}, function(err, campgrounds) {
+		if (err) {
 			console.log(err);
 		} else {
-			res.render("campgrounds", {campgrounds: campgrounds});
+			res.render('index', { campgrounds: campgrounds });
 		}
 	});
 });
@@ -32,9 +50,10 @@ app.post('/campgrounds', function(req, res) {
 	// res.send('Hello bro');
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
-	Campground.create(newCampground, function(err, newlyCreated){
-		if(err) {
+	var desc = req.body.description;
+	var newCampground = { name: name, image: image, description: desc };
+	Campground.create(newCampground, function(err, newlyCreated) {
+		if (err) {
 			console.log('err');
 		} else {
 			res.redirect('/campgrounds');
@@ -42,8 +61,18 @@ app.post('/campgrounds', function(req, res) {
 	});
 });
 
-app.get('/campgrounds/new', function(req, res){
+app.get('/campgrounds/new', function(req, res) {
 	res.render('new');
+});
+
+app.get('/campgrounds/:id', function(req, res) {
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('show', { campground: foundCampground });
+		}
+	});
 });
 
 app.listen(3000, function() {
