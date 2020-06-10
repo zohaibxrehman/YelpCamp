9575ff7d3adda81bd13dabe780fac24bdb369a33
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-10 16:34:15 -0400

    Refactored mongo models to own directory

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-
-var Campground = mongoose.model('Campground', campgroundSchema);
 
 // Campground.create(
 // 	{
