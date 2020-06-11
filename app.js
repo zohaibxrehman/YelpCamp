372ed36d6b5351f033650cf9cca266e058bdbe91
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-11 00:27:11 -0400

    Added new comment functionality

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
 	seed = require('./seed'),
+	Comment = require('./models/comment'),
 	Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-seed();
+// seed();
 
 app.get('/', function(req, res) {
 	res.render('landing');
@@ -59,6 +60,36 @@ app.get('/campgrounds/:id', function(req, res) {
 });
 
 // COMMENT ROUTES
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+	// find campground by id
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('comments/new', { campground: campground });
+		}
+	});
+});
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+	//lookup campground using ID
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err);
+			res.redirect('/campgrounds');
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if (err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect('/campgrounds/' + campground._id);
+				}
+			});
+		}
+	});
+});
 
 app.listen(3000, function() {
 	console.log('The YelpCamp serving on PORT 3000');
