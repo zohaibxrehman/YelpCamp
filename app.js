d9cd82b1a006252806fef47f609f925d6b1c432e
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-11 02:36:24 -0400

    Added User model. Added register auth and page

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local'),
 	seed = require('./seed'),
 	Comment = require('./models/comment'),
+	User = require('./models/user'),
 	Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Hello123",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // seed();
 
@@ -89,6 +105,25 @@ app.post('/campgrounds/:id/comments', function(req, res) {
 				}
 			});
 		}
+	});
+});
+
+// show register form
+app.get("/register", function(req, res){
+	res.render("register"); 
+ });
+
+ // register logic
+app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+		res.redirect("/campgrounds"); 
+		});
 	});
 });
 
