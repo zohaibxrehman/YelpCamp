c624db0664ca7e6b4149f85a3e356637613e610e
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-13 03:31:41 -0400

    New camp route now supports price.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,12 +19,13 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
 	// res.send('Hello bro');
 	var name = req.body.name;
 	var image = req.body.image;
+	var price = req.body.price;
 	var desc = req.body.description;
 	var author = {
 		id: req.user._id,
 		username: req.user.username
 	};
-	var newCampground = { name: name, image: image, description: desc, author: author };
+	var newCampground = { name: name, price: price, image: image, description: desc, author: author };
 	Campground.create(newCampground, function(err, newlyCreated) {
 		if (err) {
 			req.flash("error", err.message);
